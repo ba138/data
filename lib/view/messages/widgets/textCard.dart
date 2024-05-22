782c75a83fe4c454e308383ed7ab63cc6d7dd7f1
Basit ali <basitalyshah51214@gmail.com>
2024-05-22 05:40:47 -0700

    changes to fix the message card

--- lib/view/messages/widgets/textCard.dart
+++ lib/view/messages/widgets/textCard.dart
@@ -11,57 +11,27 @@ class TextCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 110,
       width: double.infinity,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16.0),
           color: const Color(0xff2B2C4E)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Center(
-              child: Text.rich(
-                TextSpan(
-                  text: 'Lorem ipsum dolor sit amet, consec.Lorem\n',
-                  style: GoogleFonts.getFont(
-                    "Poppins",
-                    textStyle: const TextStyle(
-                      color: AppColor.whiteColor,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'Lorem ipsum dolor sit amet,\n',
-                      style: GoogleFonts.getFont(
-                        "Poppins",
-                        textStyle: const TextStyle(
-                          color: AppColor.whiteColor,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Lorem ipsum dolor sit amet,ipsum\n',
-                      style: GoogleFonts.getFont(
-                        "Poppins",
-                        textStyle: const TextStyle(
-                          color: AppColor.whiteColor,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ],
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Lorem ipsum dolor sit amet, consec.Lorem Lorem ipsum dolor sit amet, consec.Lorem Lorem ipsum dolor sit amet, consec.Lorem Lorem ipsum dolor sit amet, consec.Lorem",
+              style: GoogleFonts.getFont(
+                "Poppins",
+                textStyle: const TextStyle(
+                  color: AppColor.whiteColor,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w400,
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
