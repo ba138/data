06d339a16eea396c9e8b9676ab22de4eab35a6f2
Basit ali <basitalyshah51214@gmail.com>
2024-05-22 05:36:04 -0700

    changes to add missing data

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           elevation: 0,
         ),
       ),
-      initialRoute: RoutesName.home,
+      initialRoute: RoutesName.splash,
       onGenerateRoute: Routes.generateRoute,
     );
   }
