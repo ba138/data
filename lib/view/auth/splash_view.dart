caf5a17164a3393571e0fc14ea10fd2fff0e4366
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-05-26 13:53:51 +0500

    working on withdraw request

--- lib/view/auth/splash_view.dart
+++ lib/view/auth/splash_view.dart
@@ -2,8 +2,12 @@ import 'dart:async';
 
 import 'package:data/Res/components/colors.dart';
 import 'package:data/utils/routes/routes_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../../utils/routes/utils.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,19 +17,26 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void startTimer() async {
+    await Future.delayed(const Duration(seconds: 5), () async {
+      try {
+        await Firebase.initializeApp();
+        if (FirebaseAuth.instance.currentUser != null) {
+          await Navigator.pushNamed(context, RoutesName.home);
+        } else {
+          await Navigator.pushNamed(context, RoutesName.logoIn);
+        }
+      } catch (e) {
+        Utils.flushBarErrorMessage(
+            '$e', context); // Handle Firebase initialization error
+      }
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     startTimer();
-  }
-
-  void startTimer() {
-    Timer(const Duration(seconds: 6), () async {
-      Navigator.pushNamed(
-        context,
-        RoutesName.logoIn,
-      );
-    });
   }
 
   @override
--- lib/view/profile/profile.dart
+++ lib/view/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:data/Res/components/colors.dart';
+import 'package:data/Res/components/loading_manager.dart';
 import 'package:data/Res/components/vertical_speacing.dart';
 import 'package:data/utils/routes/routes_name.dart';
 import 'package:data/view/profile/widget/profile_text_field.dart';
@@ -93,116 +94,120 @@ class _ProfileScreenState extends State<ProfileScreen> {
           },
         ),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const VerticalSpeacing(10),
-              Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundColor: AppColor.whiteColor,
-                    backgroundImage: NetworkImage(
-                      _pImage ?? defaultProfile,
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 2,
-                    child: Container(
-                      height: 22,
-                      width: 22,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: AppColor.buttonColor),
-                      child: const Center(
-                          child: Icon(
-                        Icons.edit_note_outlined,
-                        color: AppColor.whiteColor,
-                        size: 14,
-                      )),
-                    ),
-                  ),
-                ],
-              ),
-              const VerticalSpeacing(16.0),
-              Text.rich(
-                TextSpan(
-                    text: '${_name ?? 'Default Name'}\n',
-                    style: GoogleFonts.getFont(
-                      "Poppins",
-                      textStyle: const TextStyle(
-                        color: AppColor.whiteColor,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w400,
+      body: LoadingManager(
+        isLoading: _isLoading,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const VerticalSpeacing(10),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: AppColor.whiteColor,
+                      backgroundImage: NetworkImage(
+                        _pImage ?? defaultProfile,
                       ),
                     ),
-                    children: [
-                      TextSpan(
-                        text: _email!.length > 12
-                            ? "${_email!.substring(0, 12)}..."
-                            : _email ?? 'Default Email',
-                        style: GoogleFonts.getFont(
-                          "Poppins",
-                          textStyle: const TextStyle(
-                            color: AppColor.whiteColor,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w400,
-                          ),
+                    Positioned(
+                      bottom: 0,
+                      right: 2,
+                      child: Container(
+                        height: 22,
+                        width: 22,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppColor.buttonColor),
+                        child: const Center(
+                            child: Icon(
+                          Icons.edit_note_outlined,
+                          color: AppColor.whiteColor,
+                          size: 14,
+                        )),
+                      ),
+                    ),
+                  ],
+                ),
+                const VerticalSpeacing(16.0),
+                Text.rich(
+                  TextSpan(
+                      text: '${_name ?? 'Default Name'}\n',
+                      style: GoogleFonts.getFont(
+                        "Poppins",
+                        textStyle: const TextStyle(
+                          color: AppColor.whiteColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                    ]),
-              ),
-              const VerticalSpeacing(46),
-              ProfileWidgets(
-                  ontap: () {
-                    Navigator.pushNamed(
-                      context,
-                      RoutesName.myprofileview,
-                    );
-                  },
-                  tColor: const Color(0xff40C269),
-                  bColor: const Color(0xffCDFF9D),
-                  icon: Icons.person_outline,
-                  trIcon: Icons.arrow_forward_ios,
-                  title: 'My Profile'),
-              const Divider(),
-              ProfileWidgets(
-                  ontap: () {
-                    Navigator.pushNamed(context, RoutesName.mymessages);
-                  },
-                  tColor: const Color(0xff46C5CA),
-                  bColor: const Color(0xff6DF5FC),
-                  icon: Icons.message_outlined,
-                  trIcon: Icons.arrow_forward_ios,
-                  title: 'My Messages'),
-              const Divider(),
-              ProfileWidgets(
-                  ontap: () {
-                    Navigator.pushNamed(context, RoutesName.mySubscribtions);
-                  },
-                  tColor: const Color(0xffA24ABF),
-                  bColor: const Color(0xffDF9EF5),
-                  icon: Icons.card_giftcard_outlined,
-                  trIcon: Icons.arrow_forward_ios,
-                  title: 'My subscribtions'),
-              const Divider(),
-              ProfileWidgets(
-                  ontap: () async {
-                    authInstance.signOut();
-                    Utils.toastMessage('SuccessFully LogOut');
-                    await Navigator.pushNamedAndRemoveUntil(
-                        context, RoutesName.logoIn, (route) => false);
-                  },
-                  tColor: const Color(0xffFF9CCB),
-                  bColor: const Color(0xffEC4091),
-                  icon: Icons.logout_outlined,
-                  trIcon: Icons.arrow_forward_ios,
-                  title: 'Log Out'),
-              const Divider(),
-            ],
+                      children: [
+                        TextSpan(
+                          text: _email!.length > 12
+                              ? "${_email!.substring(0, 12)}..."
+                              : _email ?? 'Default Email',
+                          style: GoogleFonts.getFont(
+                            "Poppins",
+                            textStyle: const TextStyle(
+                              color: AppColor.whiteColor,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ]),
+                ),
+                const VerticalSpeacing(46),
+                ProfileWidgets(
+                    ontap: () {
+                      Navigator.pushNamed(
+                        context,
+                        RoutesName.myprofileview,
+                      );
+                    },
+                    tColor: const Color(0xff40C269),
+                    bColor: const Color(0xffCDFF9D),
+                    icon: Icons.person_outline,
+                    trIcon: Icons.arrow_forward_ios,
+                    title: 'My Profile'),
+                const Divider(),
+                ProfileWidgets(
+                    ontap: () {
+                      Navigator.pushNamed(context, RoutesName.mymessages);
+                    },
+                    tColor: const Color(0xff46C5CA),
+                    bColor: const Color(0xff6DF5FC),
+                    icon: Icons.message_outlined,
+                    trIcon: Icons.arrow_forward_ios,
+                    title: 'My Messages'),
+                const Divider(),
+                ProfileWidgets(
+                    ontap: () {
+                      Navigator.pushNamed(context, RoutesName.mySubscribtions);
+                    },
+                    tColor: const Color(0xffA24ABF),
+                    bColor: const Color(0xffDF9EF5),
+                    icon: Icons.card_giftcard_outlined,
+                    trIcon: Icons.arrow_forward_ios,
+                    title: 'My subscribtions'),
+                const Divider(),
+                ProfileWidgets(
+                    ontap: () async {
+                      authInstance.signOut();
+                      Utils.toastMessage('SuccessFully LogOut');
+                      await Navigator.pushNamedAndRemoveUntil(
+                          context, RoutesName.logoIn, (route) => false);
+                    },
+                    tColor: const Color(0xffFF9CCB),
+                    bColor: const Color(0xffEC4091),
+                    icon: Icons.logout_outlined,
+                    trIcon: Icons.arrow_forward_ios,
+                    title: 'Log Out'),
+                const Divider(),
+              ],
+            ),
           ),
         ),
       ),
