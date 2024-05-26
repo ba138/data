1a09169dc90f5376d8ebd879678e5434e1a498a7
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-05-26 19:35:34 +0500

    edit profile done

--- lib/view/profile/edit_profile_view.dart
+++ lib/view/profile/edit_profile_view.dart
@@ -1,12 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:data/Res/components/colors.dart';
 import 'package:data/Res/components/custom_text_field.dart';
+import 'package:data/Res/components/loading_manager.dart';
 import 'package:data/Res/components/rounded_button.dart';
 import 'package:data/Res/components/vertical_speacing.dart';
+import 'package:data/utils/routes/routes_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class EditProfileView extends StatelessWidget {
+class EditProfileView extends StatefulWidget {
   const EditProfileView({super.key});
+
+  @override
+  _EditProfileViewState createState() => _EditProfileViewState();
+}
+
+class _EditProfileViewState extends State<EditProfileView> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final User? user = FirebaseAuth.instance.currentUser;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    if (user != null) {
+      try {
+        final DocumentSnapshot userDoc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .get();
+        if (userDoc.exists) {
+          _nameController.text = userDoc.get('name');
+          _emailController.text = userDoc.get('email');
+          _phoneController.text = userDoc.get('phone');
+        }
+      } catch (error) {
+        // Handle error
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+  Future<void> _updateUserData() async {
+    if (user != null) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .update({
+          'name': _nameController.text,
+          'email': _emailController.text,
+          'phone': _phoneController.text,
+        });
+        Navigator.pushNamedAndRemoveUntil(
+            context, RoutesName.home, (route) => false);
+      } catch (error) {
+        // Handle error
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,35 +107,42 @@ class EditProfileView extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const TextFieldCustom(
-              keyboardType: TextInputType.number,
-              maxLines: 1,
-              text: 'Full Name',
-              hintText: "Hiren",
+      body: LoadingManager(
+        isLoading: _isLoading,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFieldCustom(
+                  controller: _nameController,
+                  keyboardType: TextInputType.text,
+                  maxLines: 1,
+                  text: 'Full Name',
+                  hintText: "Hiren",
+                ),
+                const VerticalSpeacing(16),
+                TextFieldCustom(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  maxLines: 1,
+                  text: 'Email Address',
+                  hintText: "Hiren123@gmail.com",
+                ),
+                const VerticalSpeacing(16),
+                TextFieldCustom(
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                  maxLines: 1,
+                  text: 'Phone Number',
+                  hintText: "123456789",
+                ),
+                const VerticalSpeacing(80),
+                RoundedButton(
+                    title: "Save Profile Detail", onpress: _updateUserData),
+              ],
             ),
-            const VerticalSpeacing(16),
-            const TextFieldCustom(
-              keyboardType: TextInputType.number,
-              maxLines: 1,
-              text: 'Email Address',
-              hintText: "Hiren123@gmail.com",
-            ),
-            const VerticalSpeacing(16),
-            const TextFieldCustom(
-              keyboardType: TextInputType.number,
-              maxLines: 1,
-              text: 'Phone Number',
-              hintText: "123456789",
-            ),
-            const VerticalSpeacing(
-              80,
-            ),
-            RoundedButton(title: "Save Profile Detail ", onpress: () {})
-          ],
+          ),
         ),
       ),
     );
--- lib/view/profile/my_profile_view.dart
+++ lib/view/profile/my_profile_view.dart
@@ -1,13 +1,72 @@
+// ignore_for_file: unnecessary_null_comparison
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:data/Res/components/colors.dart';
+import 'package:data/Res/components/loading_manager.dart';
 import 'package:data/Res/components/rounded_button.dart';
 import 'package:data/Res/components/vertical_speacing.dart';
 import 'package:data/utils/routes/routes_name.dart';
 import 'package:data/view/profile/widget/my_profile_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyProfileView extends StatelessWidget {
+class MyProfileView extends StatefulWidget {
   const MyProfileView({super.key});
+
+  @override
+  State<MyProfileView> createState() => _MyProfileViewState();
+}
+
+class _MyProfileViewState extends State<MyProfileView> {
+  final authInstance = FirebaseAuth.instance;
+  String? _email;
+  String? _name;
+  String? _phone;
+  bool _isLoading = false;
+  final User? user = FirebaseAuth.instance.currentUser;
+  String defaultProfile =
+      'https://t4.ftcdn.net/jpg/00/64/67/27/360_F_64672736_U5kpdGs9keUll8CRQ3p3YaEv2M6qkVY5.jpg';
+  @override
+  void initState() {
+    getUserData();
+    super.initState();
+  }
+
+  Future<void> getUserData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final googleAuth = FirebaseAuth.instance.currentUser;
+    _name = googleAuth?.displayName ?? 'You';
+    _email = googleAuth?.email ?? 'ID: 5788478';
+    setState(() {
+      _isLoading = false;
+    });
+    if (user != null) {
+      try {
+        String _uid = user!.uid;
+        final DocumentSnapshot userDoc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(_uid)
+            .get();
+        if (userDoc != null || userDoc.data() != null) {
+          _email = userDoc.get('email');
+          _name = userDoc.get('name');
+          _phone = userDoc.get('phone');
+        }
+      } catch (error) {
+        setState(() {
+          _isLoading = false;
+        });
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,37 +93,40 @@ class MyProfileView extends StatelessWidget {
           },
         ),
       ),
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const MyProfileWidget(
-              title: 'Full Name',
-              subTitle: 'Hiran',
-            ),
-            const VerticalSpeacing(20),
-            const MyProfileWidget(
-              title: 'Gmail Address',
-              subTitle: 'Hiran123@gmail.com',
-            ),
-            const VerticalSpeacing(20),
-            const MyProfileWidget(
-              title: 'Phone Number',
-              subTitle: '123456789',
-            ),
-            const VerticalSpeacing(80),
-            RoundedButton(
-                title: "Edit Profile",
-                onpress: () {
-                  Navigator.pushNamed(
-                    context,
-                    RoutesName.editprofileview,
-                  );
-                })
-          ],
-        ),
-      )),
+      body: LoadingManager(
+        isLoading: _isLoading,
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              MyProfileWidget(
+                title: 'Full Name',
+                subTitle: '${_name ?? 'Default Name'}\n',
+              ),
+              const VerticalSpeacing(20),
+              MyProfileWidget(
+                title: 'Email Address',
+                subTitle: '${_email ?? 'email null'}\n',
+              ),
+              const VerticalSpeacing(20),
+              MyProfileWidget(
+                title: 'Phone Number',
+                subTitle: '${_phone ?? 'phone no null'}\n',
+              ),
+              const VerticalSpeacing(80),
+              RoundedButton(
+                  title: "Edit Profile",
+                  onpress: () {
+                    Navigator.pushNamed(
+                      context,
+                      RoutesName.editprofileview,
+                    );
+                  })
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
