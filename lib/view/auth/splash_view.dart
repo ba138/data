5af614d354c3c6cd725923dbc13c69962ca81a5f
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-05-26 22:40:22 +0500

    handle user session

--- lib/view/auth/splash_view.dart
+++ lib/view/auth/splash_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:data/Res/components/colors.dart';
 import 'package:data/utils/routes/routes_name.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -21,16 +22,53 @@ class _SplashScreenState extends State<SplashScreen> {
     await Future.delayed(const Duration(seconds: 5), () async {
       try {
         await Firebase.initializeApp();
-        if (FirebaseAuth.instance.currentUser != null) {
-          await Navigator.pushNamed(context, RoutesName.home);
+        User? user = FirebaseAuth.instance.currentUser;
+
+        if (user != null) {
+          DocumentSnapshot userDoc = await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .get();
+
+          if (userDoc.exists) {
+            bool isBlock = userDoc['isBlock'];
+
+            if (isBlock) {
+              showBlockedPopup();
+            } else {
+              await Navigator.pushNamed(context, RoutesName.home);
+            }
+          } else {
+            Utils.flushBarErrorMessage('User data not found', context);
+          }
         } else {
           await Navigator.pushNamed(context, RoutesName.logoIn);
         }
       } catch (e) {
-        Utils.flushBarErrorMessage(
-            '$e', context); // Handle Firebase initialization error
+        Utils.flushBarErrorMessage('$e', context);
       }
     });
+  }
+
+  void showBlockedPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Account Blocked"),
+          content: const Text(
+              "Your account has been blocked. Please contact support."),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
